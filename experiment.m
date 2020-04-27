3ed4793057e01a11ed076af6606f5a74648c719b
Kaho-Lee <ljh243771593@hotmail.com>
2020-04-27 23:38:17 +0100

    Add one-class svm for anomaly detection

--- experiment.m
+++ experiment.m
@@ -6,11 +6,16 @@ inds = ~strcmp(species,'setosa');
 X = meas(inds,3:4);
 y_cat = species(inds);
 
+% X = X(1:50,:);
+% y_cat = y_cat(1:50,:);
+
 y = grp2idx(y_cat);
 
 y(y == 1) = -1;
 y(y == 2) = 1;
 C=1000;
+
+
 s = svmOpt(X, y, C)
 
 a = zeros( 100, 1);
@@ -25,37 +30,71 @@ x2 = linspace(min(X(:,2))-0.5, max(X(:,2))+0.5, n+1);
 score = s.predict([x1_grid(:), x2_grid(:)]);
 out = reshape(score,size(x1_grid,1),size(x2_grid,2));
 
-pred = (s.predict(X) == y);
+%pred = (s.predict(X) == y);
 
-%margin = 
-
-figure, contour(x1_grid,x2_grid, out, [0,0], 'ShowText','on')
+figure, contour(x1_grid,x2_grid, out, [0,0] ,'ShowText','on')
 hold on
 contour(x1_grid,x2_grid, out, [ [1,1], [-1,-1]], 'k-.', 'ShowText','on')
 gscatter(X(:,1),X(:,2),y_cat)
 plot(s.SVs(:,1),s.SVs(:,2),'ko','MarkerSize',10)
+legend('hyper-plane','margin','-1','1','Support Vector')
+hold off
+
+%outlier
+
+load fisheriris
+X_nonlin = meas(:,1:2);
+y_nonlin = ones(size(X_nonlin,1),1);
+C=10000;
+for i=1:length(X_nonlin(1,:))                    
+    X_nonlin(:,i) = (X_nonlin(:,i) - mean(X_nonlin(:,i)))/sqrt(var(X_nonlin(:,i)));
+end
+s_outlier = svmOpt(X_nonlin, y_nonlin, C, 0.05)
+
+n = 300;
+x1 = linspace(min(X_nonlin(:,1))-0.5, max(X_nonlin(:,1))+0.5, n+1);
+x2 = linspace(min(X_nonlin(:,2))-0.5, max(X_nonlin(:,2))+0.5, n+1);
+[x1_grid, x2_grid] = meshgrid(x1,x2);
+score = s_outlier.predict([x1_grid(:), x2_grid(:)]);
+out = reshape(score,size(x1_grid,1),size(x2_grid,2));
+
+figure, contour(x1_grid,x2_grid, out, [0,0], 'ShowText','on')
+hold on
+contour(x1_grid,x2_grid, out, 'ShowText','on')
+plot(X_nonlin(:,1),X_nonlin(:,2), 'k.')
+plot(s_outlier.SVs(:,1),s_outlier.SVs(:,2),'ko','MarkerSize',10)
 % legend('hyper-plane', 'Support Vecotr')
 hold off
 
-%%
-%non-linear
+%% Experiment in https://uk.mathworks.com/help/stats/support-vector-machines-for-binary-classification.html
+rng(1); % For reproducibility
+r = sqrt(rand(100,1)); % Radius
+t = 2*pi*rand(100,1);  % Angle
+data1 = [r.*cos(t), r.*sin(t)]; % Points
 
-% load fisheriris
-% X_nonlin = meas(:,1:2);
-% y_nonlin = ones(size(X_nonlin,1),1);
-% C=100;
-% s = svmOpt(X_nonlin, y_nonlin, C)
-% 
-% n = 300;
-% x1 = linspace(min(X_nonlin(:,1))-0.5, max(X_nonlin(:,1))+0.5, n+1);
-% x2 = linspace(min(X_nonlin(:,2))-0.5, max(X_nonlin(:,2))+0.5, n+1);
-% [x1_grid, x2_grid] = meshgrid(x1,x2);
-% score = s.predict([x1_grid(:), x2_grid(:)]);
-% out = reshape(score,size(x1_grid,1),size(x2_grid,2));
-% 
-% figure, contour(x1_grid,x2_grid, out, 'ShowText','on')
-% hold on
-% plot(X_nonlin(:,1),X_nonlin(:,2), 'k.')
-% plot(s.SVs(:,1),s.SVs(:,2),'ko','MarkerSize',10)
-% % legend('hyper-plane', 'Support Vecotr')
-% hold off
+r2 = sqrt(3*rand(100,1)+1); % Radius
+t2 = 2*pi*rand(100,1);      % Angle
+data2 = [r2.*cos(t2), r2.*sin(t2)]; % points
+
+data3 = [data1;data2];
+theclass = ones(200,1);
+theclass(1:100) = -1;
+
+s_nonlin = svmOpt(data3, theclass, C)
+
+n = 300;
+x1 = linspace(min(data3(:,1))-0.5, max(data3(:,1))+0.5, n+1);
+x2 = linspace(min(data3(:,2))-0.5, max(data3(:,2))+0.5, n+1);
+[x1_grid, x2_grid] = meshgrid(x1,x2);
+score = s_nonlin.predict([x1_grid(:), x2_grid(:)]);
+out = reshape(score,size(x1_grid,1),size(x2_grid,2));
+
+%pred = (s.predict(X) == y);
+
+figure, contour(x1_grid,x2_grid, out, [0,0], 'ShowText','on')
+hold on
+contour(x1_grid,x2_grid, out, [ [1,1], [-1,-1]], 'k-.', 'ShowText','on')
+gscatter(data3(:,1),data3(:,2),theclass)
+plot(s_nonlin.SVs(:,1),s_nonlin.SVs(:,2),'ko','MarkerSize',10)
+legend('hyper-plane','margin','-1','1','Support Vector')
+hold off
--- svmOpt.m
+++ svmOpt.m
@@ -12,6 +12,8 @@ classdef svmOpt
         W;
         b;
         kernel;
+        numclass;
+        outlierFraction;
     end
     
     methods
@@ -20,10 +22,11 @@ classdef svmOpt
             obj.y = y;
             size_X = size(obj.X);
             obj.size = size_X(1);
+            obj.numclass = length(unique(y));
             
-            obj.kernel = @(x1, x2) x1 * (x2.');
-            %var = 1;
-            %obj.kernel = @(x1, x2) exp(-norm(x1-x2)/(2*var));
+            %obj.kernel = @(x1, x2) x1 * (x2.');
+            sigma = 1;
+            obj.kernel = @(x1, x2) exp(-norm(x1-x2)/(2*sigma));
             
             H = setH(obj);
             obj.H = H;
@@ -31,15 +34,19 @@ classdef svmOpt
             if nargin==2
                 % third parameter does not exist, so default it to something
                 obj.C = 0;
-            else
+            elseif nargin==3
                 obj.C = varargin{1};
+                obj.outlierFraction = 0;
+            elseif nargin==4
+                obj.C = 1/(length(obj.y)*varargin{2});
+                obj.outlierFraction = varargin{2};
             end
+            
                         
             obj.A = zeros(obj.size, 1);
             %quadratic penalty objective function, dual form
             
             obj.L = QudraticPenalty(obj);
-            %obj.L = AugmentedLagrangian(obj);
             
             obj.A = fit(obj);
             [obj.SVs, obj.isSV] = assignSupportVecotor(obj);
@@ -50,60 +57,70 @@ classdef svmOpt
         function aMin = fit(obj)
             mu0 = 1;
             a0 = obj.A;
-            [aMin, fMin_QP, nIter_QP, info_QP] = PenaltyAugmented(obj.L, mu0, a0, 'QuadraticPenalty');
-                       
-            %v_0 = [1 1 1];
-            %c_1 = @(a) [a.' * obj.y, sum(max(0, 0-a), 'all'), sum(max(0, a-obj.C), 'all')];
-            %[aMin, fMin_LA, nIter_LA, info_LA] = PenaltyAugmented(obj.L, mu0, a0, 'AugmentedLagrangian', v_0, c_1);
+            [aMin, fMin_QP, nIter_QP, info_QP] = PenaltyAugmented(obj.L, mu0, a0, 'QuadraticPenalty');                       
             disp('finish training')
         end
         
         function pred = predict(obj, X)
             logits = [];
             
-            for s=1:length(X)
-                tot_sum = 0;
-                for m=1:obj.size
-                   tot_sum = tot_sum + obj.A(m) .* obj.y(m) .* obj.kernel(obj.X(m,:), X(s,:));
+            if obj.outlierFraction == 0                 
+                for s=1:length(X)
+                    tot_sum = 0;
+                    for m=1:obj.size
+                       tot_sum = tot_sum + obj.A(m) .* obj.y(m) .* obj.kernel(obj.X(m,:), X(s,:));
+                    end
+                    logits = [logits, tot_sum+obj.b];
                 end
-                logits = [logits, tot_sum+obj.b];
+            elseif obj.outlierFraction > 0
+                disp('2 predict')
+                for s=1:length(X)
+                    tot_sum = 0;
+                    for m=1:obj.size
+                       tot_sum = tot_sum + obj.A(m) .* obj.kernel(obj.X(m,:), X(s,:));
+                    end
+                    logits = [logits, tot_sum-obj.b];
+                end
             end
             pred = logits;
         end
         
-        function L = AugmentedLagrangian(obj)
-            %Augmented Lagrangian
-            L.f = @(a, mu, v) 0.5*sum((a * a.').*obj.H, 'all') - sum(a, 'all') ...
-                - v(1)*a.' * obj.y - v(2)*sum(max(0, a), 'all') - v(3)*sum(max(0, obj.C-a), 'all') ...
-                + (mu/2)*((a.' * obj.y).^2 + sum(max(0, 0-a).^2, 'all') ...
-                + sum(max(0, a-obj.C).^2, 'all') );
-            str = '@(a, mu, v, H, y, C)[';
-            for i=1:obj.size
-                df_i = sprintf("0.5*sum(a.'.*H(%d,:), 'all')-1-v(1)*y(%d)+v(2)*heaviside(a(%d))-v(3)*heaviside(C-a(%d))+mu*((a.' * y)*y(%d)+min(0, a(%d))+max(0, a(%d)-C));",i,i,i,i,i,i,i);
-                str = strcat(str,df_i);
-            end
-            str = strcat(str,"];");
-            df = str2func(str);
-            L.df = @(a, mu, v) df(a, mu,v, obj.H, obj.y, obj.C);
-        end
         
         function L = QudraticPenalty(obj)
-            L.f = @(a, mu) 0.5*sum((a * a.').*obj.H, 'all') - sum(a, 'all') ...
-                + (mu/2)*((a.' * obj.y).^2 + sum(max(0, 0-a).^2, 'all') ...
-                + sum(max(0, a-obj.C).^2, 'all') );
             
-            str = '@(a, mu, H, y, C)[';
-            for i=1:obj.size
-                df_i = sprintf("0.5*sum(a.'.*H(%d,:), 'all') - 1 + mu*((a.' * y)*y(%d)+min(0, a(%d))+max(0, a(%d)-C));",i,i,i,i);
-                str = strcat(str,df_i);
+            if obj.outlierFraction == 0           
+                L.f = @(a, mu) 0.5*sum((a * a.').*obj.H, 'all') - sum(a, 'all') ...
+                    + (mu/2)*((a.' * obj.y).^2 + sum(max(0, 0-a).^2, 'all') ...
+                    + sum(max(0, a-obj.C).^2, 'all') );
+
+                str = '@(a, mu, H, y, C)[';
+                for i=1:obj.size
+                    df_i = sprintf("0.5*sum(a.'.*H(%d,:), 'all') - 1 + mu*((a.' * y)*y(%d)+min(0, a(%d))+max(0, a(%d)-C));",i,i,i,i);
+                    str = strcat(str,df_i);
+                end
+                str = strcat(str,"];");
+                df = str2func(str);
+                L.df = @(a, mu) df(a, mu, obj.H, obj.y, obj.C);
+            elseif obj.outlierFraction > 0
+                disp('2 Quadratic penalty')
+                L.f = @(a, mu) 0.5*sum((a * a.').*obj.H, 'all') ...
+                    + (mu/2)*((sum(a,'all')-1).^2 + sum(max(0, 0-a).^2, 'all') ...
+                    + sum(max(0, a-obj.C).^2, 'all') );
+                
+                str = '@(a, mu, H, C)[';
+                for i=1:obj.size
+                    df_i = sprintf("0.5*sum(a.'.*H(%d,:), 'all') + mu*((sum(a,'all')-1)+min(0, a(%d))+max(0, a(%d)-C));",i,i,i);
+                    str = strcat(str,df_i);
+                end
+                str = strcat(str,"];");
+                df = str2func(str);
+                L.df = @(a, mu) df(a, mu, obj.H, obj.C);
             end
-            str = strcat(str,"];");
-            df = str2func(str);
-            L.df = @(a, mu) df(a, mu, obj.H, obj.y, obj.C);
         end
                 
         function H = setH(obj)            
             H = zeros(obj.size, obj.size);
+            
             for i=1:obj.size
                 for j=1:obj.size
 %                     H(i,j) = obj.y(i)* obj.y(j)*(obj.X(i,:) * obj.X(j,:).');                                
@@ -136,31 +153,42 @@ classdef svmOpt
             end                                        
         end 
         
-        function W = setW(obj)
-            
-            a_y = obj.A .* obj.y;
-            W = a_y .* obj.X;
-            W = sum(W, 1);
-        end
-        
-        function b = setBias(obj)            
-%             a_y_x_m = sum( obj.A .* obj.y .* obj.kernel(obj.X, x_s), 1); %1x2 w = sum(a*y*X)
-                                   
-            y_s = obj.isSV .* obj.y; % 100x1
-            
-            x_s = (obj.isSV .* obj.X); %2x100
-            %temp = sum( obj.A .* obj.y .* obj.kernel(obj.X, x_s), 1); %1x100 w = sum(a*y*x_n*x_s)
+        function b = setBias(obj)               
             temp = [];
-            for s=1:obj.size
-                tot_sum = 0;
-                for m=1:obj.size
-                   tot_sum = tot_sum + obj.A(m) .* obj.y(m) .* obj.kernel(obj.X(m,:), x_s(s,:));
+            y_s = obj.isSV .* obj.y; % 100x1            
+            x_s = (obj.isSV .* obj.X); %2x100
+            if obj.outlierFraction == 0                   
+                temp = [];
+                for s=1:obj.size
+                    tot_sum = 0;
+                    if x_s(s,1) == 0 && x_s(s,2) == 0
+                        temp = [temp 0];
+                       continue
+                    end
+                    for m=1:obj.size
+                       tot_sum = tot_sum + obj.A(m) .* obj.y(m) .* obj.kernel(obj.X(m,:), x_s(s,:));
+                    end
+                    temp = [temp tot_sum];
                 end
-                temp = [temp tot_sum];
+                b = sum(y_s - temp.')/sum(obj.isSV);   
+            elseif obj.outlierFraction > 0 
+                disp('2 bias')
+                for s=1:obj.size
+                    tot_sum = 0;
+                    if x_s(s,1) == 0 && x_s(s,2) == 0
+                        temp = [temp 0];
+                        continue
+                    end
+                    for m=1:obj.size
+
+                       tot_sum = tot_sum + y_s(s).*obj.A(m) .* obj.kernel(obj.X(m,:), x_s(s,:));
+                    end
+                    
+                    temp = [temp tot_sum];
+                    %break
+                end
+                b = sum(temp)/sum(obj.isSV);                                
             end
-            
-%             temp = a_y_x_m * x_s; %1x100
-            b = sum(y_s - temp.')/sum(obj.isSV);             
         end
         
     end
